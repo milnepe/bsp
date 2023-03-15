3262c2fb78d961ec2b15c4098cdd5045a36e0347
Peter Milne <milnepe@gmail.com>
2023-03-15 09:50:40 +0000

    Update overlay checksum

--- linux/.common/overlays.sh
+++ linux/.common/overlays.sh
@@ -1,4 +1,9 @@
 custom_source_action() {
-    git_source https://github.com/radxa/overlays.git 1ad6778a2f8c14a92e6a61157791655dbc5d1727
+    #git_source https://github.com/radxa/overlays.git 1ad6778a2f8c14a92e6a61157791655dbc5d1727
+    #cp -r $SCRIPT_DIR/.src/overlays/arch $TARGET_DIR
+
+    git_source https://github.com/milnepe/overlays.git c50702d57c30c0ac2121ef540dcd37ccae362cdf
     cp -r $SCRIPT_DIR/.src/overlays/arch $TARGET_DIR
+
 }
+
